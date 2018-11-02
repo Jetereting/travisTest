c109f6db1b9e0c800be4a9f96419d19bd2eb2bfc
Au <1570967070@qq.com>
2018-11-02 16:25:19 +0800

    up

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/astaxie/beego/httplib"
+	"fmt"
+)
 
 func main() {
+
+	ip,_:=httplib.Get("http://myexternalip.com/raw").String()
+	fmt.Println("ip:::",ip)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		ip,_:=httplib.Get("http://myexternalip.com/raw").String()
+
 		c.JSON(200, gin.H{
 			"message": "pong3",
+			"ip": ip,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
